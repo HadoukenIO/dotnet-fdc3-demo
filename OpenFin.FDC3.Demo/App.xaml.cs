78a069f226c7d82082cd46db0b25864018e65c6e
Les <leslie@openfin.co>
2020-01-08 15:48:45 -0500

    Add project files.

---
+++ OpenFin.FDC3.Demo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace OpenFin.FDC3.Demo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
